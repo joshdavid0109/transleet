65a7111fbeaad831d244990491245d27a24b3c08
franzxsu <2225292@slu.edu.ph>
2024-04-30 03:52:56 +0800

    main

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
-ilo_en_tokenizer = AutoTokenizer.from_pretrained("models/opus-mt-ilo-en")
-ilo_en_model = AutoModelForSeq2SeqLM.from_pretrained("models/opus-mt-ilo-en")
+def load_model_and_tokenizer(model_name):
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+    return tokenizer, model
 
-en_ilo_tokenizer = AutoTokenizer.from_pretrained("models/opus-mt-en-ilo")
-en_ilo_model = AutoModelForSeq2SeqLM.from_pretrained("models/opus-mt-en-ilo")
+def translate_text(tokenizer, model, text):
+    input_ids = tokenizer(text, return_tensors="pt")["input_ids"]
+    generated_sequence = model.generate(input_ids=input_ids)[0].numpy().tolist()
+    translated_text = tokenizer.decode(generated_sequence, skip_special_tokens=True)
+    return translated_text
 
-#TEST ILO TO EN
-sentence_ilo="mayat a agsapa"
+#load models and tokenizers
+ilo_en_tokenizer, ilo_en_model = load_model_and_tokenizer("models/opus-mt-ilo-en")
+en_ilo_tokenizer, en_ilo_model = load_model_and_tokenizer("models/opus-mt-en-ilo")
 
-input_ids=ilo_en_tokenizer(sentence_ilo, return_tensors="pt")["input_ids"]
-generated_sequence = ilo_en_model.generate(input_ids=input_ids)[0].numpy().tolist()
+def eng_to_ilo(text):
+    return translate_text(en_ilo_tokenizer, en_ilo_model, text)
 
-english_sentence=ilo_en_tokenizer.decode(generated_sequence, skip_special_tokens=True)
-print(english_sentence)
+def ilo_to_eng(text):
+    return translate_text(ilo_en_tokenizer, ilo_en_model, text)
 
-#TEST EN TO ILO
-sentence_en="good morning"
 
-input_ids=en_ilo_tokenizer(sentence_en, return_tensors="pt")["input_ids"]
-generated_sequence = en_ilo_model.generate(input_ids=input_ids)[0].numpy().tolist()
+# TEST
+sentence_ilo = "mayat a agsapa"
+english_sentence = eng_to_ilo(sentence_ilo)
+print("ILO-EN:", english_sentence)
 
-ilocano_sentence=en_ilo_tokenizer.decode(generated_sequence, skip_special_tokens=True)
-print(ilocano_sentence)
+sentence_en = "good morning"
+ilocano_sentence = ilo_to_eng(sentence_en)
+print("EN-ILO:", ilocano_sentence)
